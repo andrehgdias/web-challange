15ed2852e6f9938147d73acb76f02a7b4a7a1cdf
andrehgdias <andredias.us@gmail.com>
2019-01-26 12:36:12 -0200

    Botão de nova tarefa transformado em um componente(BtnNewTask) e está recebendo uma função newTask que irá salvar a nova tarefa criada no modal.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import TaskColumn from './components/TaskColumn/TaskColumn';
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row" style={{'padding-top': '50px'}}>
+      <div className="container" style={{'marginTop': '50px'}}>
+        <div className="row" >
           <TaskColumn title="Disponível"/>
           <TaskColumn title="Em andamento"/>
           <TaskColumn title="Concluído"/>
---
+++ src/components/BtnNewTask/BtnNewTask.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BtnNewTask = props => (
+        <button className="btn btn-primary" onClick={props.callModal}>Nova Tarefa</button>
+);
+
+export default BtnNewTask;
