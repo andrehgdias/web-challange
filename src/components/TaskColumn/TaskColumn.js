335adc19fc67783939d726b9181569ad6d7145e8
andrehgdias <andredias.us@gmail.com>
2019-01-26 12:36:12 -0200

    Botão de nova tarefa transformado em um componente(BtnNewTask) e está recebendo uma função callModal que irá chamar o modal para registrar a nova tarefa

--- src/components/TaskColumn/TaskColumn.js
+++ src/components/TaskColumn/TaskColumn.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import './TaskColumn.css';
 
+import BtnNewTask from '../BtnNewTask/BtnNewTask'
+
 class TaskColumn extends React.Component{
     state = {
-        tasks: []
-    }
+        tasks: [
+            {
+                'Título': "Primeira",
+                'Modelo':"Lenovo Ideapad 310",
+                'Cliente':"Jeferson",
+                'Descrição':"Descrição básica para testes",
+                'DataPedido': "26/01/2019"
+            }
+        ]
+    };
+
+    newTask = () => {
+        console.warn("New Task Baby!!!");
+    };
 
     render(){
         return (
-            <div className="col col-md-">
+            <div className="col col-md-4">
                 <div className="card">
                     <div className="card-header">
                         {this.props.title}        
                     </div>
 
-                    {this.props.title == "Disponível" &&
-                        <button className="btn btn-primary">Nova Tarefa</button>
+                    {this.props.title === "Disponível" &&
+                        <BtnNewTask callModal={this.newTask}/>
                     }
                 </div>
             </div>
         );
-    }
+    };
 }
 
 export default TaskColumn;
